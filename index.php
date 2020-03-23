389ae32412ef4d1bdea320fb5e7686b17dd48af7
“Milo <milogirardet@gmail.com>
2020-03-23 13:57:25 +0000

    re-committed intial html

---
+++ index.php
@@ -0,0 +1,39 @@
+<html lang='en'>
+    <head>
+        <link rel="stylesheet" href="css_stylesheet.css">
+        <title>Road Bike Collection</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body onTouchStart="">
+        <h1>Collection of Road Bikes</h1>
+        <div class="collectionContainer1">
+            <div class="collectionItem">
+                <h2>Collection Item 1</h2>
+                <img></img>
+                <ul class="informationList"></ul>
+                    <li>Name:</li>
+                    <li>Brand:</li>
+                    <li>Price:</li>
+                    <li>Class:</li>
+            </div>
+            <div class="collectionItem">
+                <h2>Collection Item 2</h2>
+                <img></img>
+                <ul class="informationList"></ul>
+                    <li>Name:</li>
+                    <li>Brand:</li>
+                    <li>Price:</li>
+                    <li>Class:</li>
+            </div>
+            <div class="collectionItem">
+                <h2>Collection Item 3</h2>
+                <img></img>
+                <ul class="informationList"></ul>
+                    <li>Name:</li>
+                    <li>Brand:</li>
+                    <li>Price:</li>
+                    <li>Class:</li>
+            </div>
+        </div>
+    </body>
+</html>
