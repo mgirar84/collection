1f06abef4a86bf351283e0d70c5f193773e4c773
“Milo <milogirardet@gmail.com>
2020-03-24 12:03:08 +0000

    updated dataquery

--- datastorequery.php
+++ datastorequery.php
@@ -1,24 +1,10 @@
 <?php
 $db = new PDO('mysql:host=db; dbname=collection', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$query = $db->prepare('SELECT `model` AS `model` FROM `roadbikes`;');
+//connect
+$query = $db->prepare('SELECT `model`, `brand`, `price`, `type`, `picture` FROM `roadbikes`;');
+//execute
 $query->execute();
-$model = $query->fetch();
+$bikes = $query->fetch();
 
-$query = $db->prepare('SELECT `brand` AS `brand` FROM `roadbikes`;');
-$query->execute();
-$brand = $query->fetch();
-
-$query = $db->prepare('SELECT `type` AS `type` FROM `roadbikes`;');
-$query->execute();
-$type = $query->fetch();
-
-$query = $db->prepare('SELECT `price` AS `price` FROM `roadbikes`;');
-$query->execute();
-$price = $query->fetch();
-
-$query = $db->prepare('SELECT `picture` AS `picture` FROM `roadbikes`;');
-$query->execute();
-$picture = $query->fetch();
 ?>
