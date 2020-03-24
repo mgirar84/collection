004a342db9b9f8772b6efc9e66549658783dce2d
“Milo <milogirardet@gmail.com>
2020-03-24 12:05:48 +0000

    formatting changes

--- datastorequery.php
+++ datastorequery.php
@@ -1,10 +1,12 @@
 <?php
+//connect
 $db = new PDO('mysql:host=db; dbname=collection', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//connect
+
+//select
 $query = $db->prepare('SELECT `model`, `brand`, `price`, `type`, `picture` FROM `roadbikes`;');
+
 //execute
 $query->execute();
 $bikes = $query->fetch();
-
 ?>
