2c8c583af7f059ba7fc4a0b9a0a3f8e8b59f7b5c
“Milo <milogirardet@gmail.com>
2020-03-24 14:25:01 +0000

    first attempt at foreach loop

--- frontendquery.php
+++ frontendquery.php
@@ -1,2 +1,11 @@
 <?php
+foreach($result as $bikes) {
+    $id = $bikes['id'];
+    $brand = $bikes['brand'];
+    $model = $bikes['model'];
+    $price = $bikes['price'];
+    $type = $bikes['type'];
+    $picture = $bikes ['picture'];
+  }
 
+  
